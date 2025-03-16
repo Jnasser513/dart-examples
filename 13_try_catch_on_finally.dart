4af0049b42232fab86efa1356bf07b71736f8b9d
jose nasser <jose.nasser@softsvgroup.com>
2025-03-15 22:59:31 -0600

    feat: upload examples using streams

--- 13_try_catch_on_finally.dart
+++ 13_try_catch_on_finally.dart
@@ -3,10 +3,16 @@ void main() async {
   print('Inicio del programa');
   
   try {
+    
     final value = await httpGet('https://github.com/Jnasser513/dart-examples');
     print( 'Exito: $value');
+    
+  } on Exception catch(err) {
+    print('Tenemos una Exception: $err');
   } catch(err) {
     print('Tenemos un error: $err');
+  } finally {
+    print('Fin del try y catch');
   }
   
   print('Fin del programa');
@@ -17,7 +23,8 @@ Future<String> httpGet( String url ) async {
   
   await Future.delayed( const Duration(seconds: 1));
   
-  throw 'Error en la peticion';
+  throw Exception('No hay parametros en el URL');
+  //throw 'Error en la peticion';
   
   //return 'Tenemos un valor de la peticion';
   
